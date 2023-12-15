947bce450e09d204e70d214814a99e99fb664e07
TerminalGambit <jackmassey.monaco@outlook.com>
2023-12-15 09:49:40 +0100

    Refactor code to improve performance and readability

---
+++ TP1/exercice2/compiler.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+gcc -Wall -ansi -pedantic exercice2.c
---
+++ TP1/exercice2/exercice1.c
@@ -0,0 +1,25 @@
+/* 
+Exercice 2 — Quelques manipulations simples
+1. Créez un fichier exercice2.c contenant une fonction question1 affichant Hello World. 
+Modifiez le fichier ./compilation.sh pour qu’il compile aussi l’exercice 2.
+
+void question1(void) {
+}
+
+int main(void) {
+    question1();
+    …
+    question6();
+    return 0 ;
+}
+*/
+#include <stdio.h>
+
+void question1(void) {
+    printf("Hello World.");
+}
+
+int main(void) {
+    question1();
+    return 0 ;
+}
