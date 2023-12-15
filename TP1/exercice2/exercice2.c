1a381e13aec9f727122d753795d23f3e651c017c
TerminalGambit <jackmassey.monaco@outlook.com>
2023-12-15 09:59:25 +0100

    Add code to display characters with ASCII codes from 65 to 90

--- TP1/exercice2/exercice2.c
+++ TP1/exercice2/exercice2.c
@@ -31,6 +31,16 @@ void question2(void) {
     printf("\n"); /* Pas nécessaire mais plus propre. */
 }
 
+/* 3. Écrivez un code C qui affiche sur une ligne les caractères dont le code ASCII va de 65 à 90 */
+
+void question3(void) {
+    int i;
+    for (i = 65; i <= 90; i++) { /* On itère de 65 à 90 inclu. */
+        printf("%c ", i); /* On affiche le caractère suivi d'un espace pour séparer. */
+    }
+    printf("\n"); /* Pas nécessaire mais plus propre. */
+}
+
 int main(void) {
     printf("Question 1 : \n");
     question1();
