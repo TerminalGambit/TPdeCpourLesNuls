c78209c362707dcee4645db6db3512c0483585fe
TerminalGambit <jackmassey.monaco@outlook.com>
2023-12-15 09:36:22 +0100

    Add compiler.sh script for automatic compilation

--- TP1/exercice1.c
+++ TP1/exercice1.c
@@ -69,5 +69,8 @@ int main(void) {
 /* Output de ./a.out :
 321 = 321
 */
-/* 4. Créer un fichier compiler.sh qui lance automatiquement la bonne commande de compilation avec les bonnes
-options pour créer un fichier exercice1.*/
+
+/* 
+4. Créer un fichier compiler.sh qui lance automatiquement la bonne commande de compilation avec les bonnes
+options pour créer un fichier exercice1.
+*/
