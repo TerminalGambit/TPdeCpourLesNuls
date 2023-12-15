c05a65276ae2079f8ad2f26c367aae339ec8302f
TerminalGambit <jackmassey.monaco@outlook.com>
2023-12-15 10:48:51 +0100

    Refactor lire_entier function to improve readability and add comments

--- TP1/exercice2/exercice3.c
+++ TP1/exercice2/exercice3.c
@@ -8,13 +8,23 @@ int lire_entier(void) {
     int i;
     char c;
     int n = 0;
-    printf("entier> ");
-    while ((c=getchar()) != '\n' && c!= EOF) {
-        i = c - '0';
-        if (i > 9 || i < 0) {
+    printf("entier> "); /* On affiche l'invite de commande. */
+    while ((c=getchar()) != '\n' && c!= EOF) { /* On lit le caractère. */
+        i = c - '0'; /* On convertit le caractère en entier. */
+        if (i > 9 || i < 0) { /* Si le caractère n'est pas un entier, on renvoie -1. */
             return -1;
         }
-        n = n*10 + i;
+        n = n * 10 + i; /* On construit l'entier. */
     }
     return n;
 }
+
+ /* Explication sur cette condition de boucle while :
+    On lit le caractère et on le stocke dans c grâce à getchar().
+    On vérifie que c est différent de '\n' et de EOF.
+    Si c'est le cas, on continue la boucle.
+    Si c'est pas le cas, on sort de la boucle.
+    EOF signifie End Of File, c'est la fin du fichier.
+    '\n' signifie retour à la ligne.
+    Donc on lit le caractère et on vérifie qu'il n'est pas égal à la fin du fichier et qu'il n'est pas égal à un retour à la ligne.
+*/
