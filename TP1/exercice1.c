6322243591b738b3943248db10ea395ff7c7a1b8
TerminalGambit <jackmassey.monaco@outlook.com>
2023-12-15 09:34:33 +0100

    Fix compilation errors and improve code readability

--- TP1/exercice1.c
+++ TP1/exercice1.c
@@ -5,6 +5,7 @@ Créez alors un fichier exercice1.c contenant le code ci-dessous.
 Vous pouvez utiliser l’éditeur de votre choix (gedit, GNU/Emacs, autre).
 */
 
+/*
 #include <stdio.h>
 
 int c = 3;
@@ -13,9 +14,10 @@ void main(void) {
     int u, d, c;
     u = 1;
     d = 2;  
-    int n = 100 * c + 10 * d + u; /* c vaut 3, d vaut 2 et u vaut 1 */
-    printf("321 = %i\n",n); /* affiche « 321 = 321 » */
+    int n = 100 * c + 10 * d + u; c vaut 3, d vaut 2 et u vaut 1
+    printf("321 = %i\n",n);  affiche « 321 = 321 » 
 }
+*/
 
 /*
 2. Dans un terminal, compilez le fichier avec la commande : « gcc exercice1.c ». Quel est le nom de l’exécutable
@@ -35,10 +37,33 @@ produit ? Lancez-le. Le résultat est-il celui attendu ?
     Qu'est-ce qui s'est passé ? 
     -- Chaque main doit forcément renvoyait un entier, donc on doit écrire int main(void).
     Pourquoi écrire void dans la parenthèse ?
-    -- On ne doit pas y mettre rien, il faut y avoir au moins 
+    -- On ne doit pas y mettre rien, il faut qu'il y est 'void' s'il n'y a rien. 
+    -> Void est un mot anglais qui se traduit comme vide.
 */
 
 /* 3. Pour éviter ce genre de mauvaise surprise, durant cette UE, nous allons systématiquement compiler le code avec
-les options « gcc -Wall -ansi -pedantic exercice1.c ». Corrigez le code jusqu’à ce que le compilateur n’affiche aucun warning et que le code s’exécute comme souhaité. Vous devez trouver trois erreurs.
-4. Créer un fichier compiler.sh qui lance automatiquement la bonne commande de compilation avec les bonnes
+les options « gcc -Wall -ansi -pedantic exercice1.c ». 
+Corrigez le code jusqu’à ce que le compilateur n’affiche aucun warning et que le code s’exécute comme souhaité. 
+Vous devez trouver trois erreurs.
+*/
+
+/* La nouvelle fonction main sans erreur : */
+
+#include <stdio.h> /* Nécessaire pour l'utilisation de la fonction print. */
+
+global_c = 3; /* On redéfinit la variable c pour ne pas se confondre. */
+
+int main(void) {
+    int u;
+    int d;
+    int c;
+    int n; /* Mettre les définitions de variables sur chaque ligne permet d'avoir plus de clarté et moins de confusion. */
+    u = 1;
+    d = 2;
+    c = 3; /* On met la valeur de c à 3 ici dans la fonction parce qu'elle ne peut pas être défini ailleurs. */
+    n = 100 * c + 10 * d + u; /* On enlève le int car dans la norme C ANSI, il faut toujours écrire les défintions de variable au début. */
+    printf("321 = %i\n", n);
+}
+
+/* 4. Créer un fichier compiler.sh qui lance automatiquement la bonne commande de compilation avec les bonnes
 options pour créer un fichier exercice1.*/
