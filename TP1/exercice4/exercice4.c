bae777ae2bc5c642a75d84c9a3aa65086667dfe5
TerminalGambit <jackmassey.monaco@outlook.com>
2023-12-15 17:23:12 +0100

    Refactor printing logic in affiche_1_a_100() function

--- TP1/exercice4/exercice4.c
+++ TP1/exercice4/exercice4.c
@@ -6,12 +6,19 @@ correctement. */
 void affiche_1_a_100(void) {
     int i;
     for (i = 1; i <= 100; i++) {
-        printf("%3d ", i);
+        printf("%3d ", i); /* On affiche le nombre */
         if (i % 10 == 0) {
             printf("\n");
         }
     }
 }
+
+/* Explication de cette ligne dans le programme :
+   printf("%3d ", i);
+   %3d signifie qu'on affiche un entier sur 3 caractères.
+   Si l'entier est inférieur à 3 caractères, on rajoute des espaces avant l'entier.
+   Si l'entier est supérieur à 3 caractères, on affiche l'entier sans espaces.
+*/
 
 int main(void) {
     affiche_1_a_100();
