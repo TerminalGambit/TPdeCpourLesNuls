a54e51301f09b77366ae11e3b8105686ea10a289
TerminalGambit <jackmassey.monaco@outlook.com>
2023-12-27 16:21:05 +0100

    Refactor code to improve performance and readability

---
+++ TP1/exercice7/exercice7.c
@@ -0,0 +1,6 @@
+/* 1. Écrivez une fonction lire_flottant sur le modèle de lire_entier.
+Un flottant est de la forme :
+(a) un signe « - » (facultatif)
+(b) une suite d’entiers représentant la partie entière.
+(si vide, la partie entière vaut 0).
+(c) un point suivie éventuellement d’une suite d’entiers représentant la partie décimale. */
