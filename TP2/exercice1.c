7ad4ba4bfb547d53e2aa67fef846c24c59c746f6
TerminalGambit <jackmassey.monaco@outlook.com>
2024-01-01 11:36:33 +0100

    Refactor affiche_tableau function to improve readability

--- TP2/exercice1.c
+++ TP2/exercice1.c
@@ -1,2 +1,4 @@
-/* 1. Écrire une fonction void affiche_tableau(int tab[], int n) qui affiche les n premiers éléments d’un tableau tab d’entiers (n devant être inférieur à la taille du tableau). Écrire une fonction question1 pour tester votre
-fonction. */
+/* 1. Écrire une fonction void affiche_tableau(int tab[], int n) 
+qui affiche les n premiers éléments d’un tableau tab d’entiers 
+(n devant être inférieur à la taille du tableau). Écrire une fonction 
+question1 pour tester votre fonction. */
