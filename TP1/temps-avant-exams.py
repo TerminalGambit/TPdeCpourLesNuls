f2acee893449a95c810319181f52ad9f1aadcc93
TerminalGambit <jackmassey.monaco@outlook.com>
2024-01-01 11:46:34 +0100

    Refactor code to calculate time left until multiple exam dates

--- TP1/temps-avant-exams.py
+++ TP1/temps-avant-exams.py
@@ -1,9 +1,14 @@
-import datetime
+from datetime import datetime
+exam_dates = [
+    "09/01/2024 13:00 Base de données",
+    "10/01/2024 13:00 Outils Formels de l'Informatique",
+    "15/01/2024 08:00 Programmation en C et structures de données",
+    "17/01/2024 08:00 Introduction à l'intelligence artificielle"
+]
 
-exam_date = datetime.datetime(2024, 1, 15, 8, 0)
-current_date = datetime.datetime.now()
+current_date = datetime.now()
 
-remaining_time = exam_date - current_date
-
-print("Time remaining until the C exam:")
-print(remaining_time)
+for exam_date in exam_dates:
+    exam_datetime = datetime.strptime(exam_date.split(" ", 1)[0], "%d/%m/%Y")
+    time_left = exam_datetime - current_date
+    print(f"Time left until {exam_date.split(' ', 1)[1]}: {time_left}")
